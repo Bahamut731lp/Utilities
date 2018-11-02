e25dfea8aa1e63baeed5d43ffe22d8e745fa1905
Bahamut731lp <bahamut731lp@gmail.com>
2018-11-02 20:08:29 +0100

    Init
    First Commit

---
+++ analyze.js
@@ -0,0 +1,19 @@
+function analyze(obj){
+	let keywords = Object.keys(obj);
+    let names = document.querySelectorAll(".p-name"), length = names.length;
+    for (let i = 0; i < length; i++){
+      let result = {}, string = names[i].textContent.trim().toLowerCase();
+      let parts = string.split(/\s/g);
+      for (let q = 0; q < parts.length; q++){
+        for (var keyword in config.keywords){
+          if (RegExp(keyword+'*').test(parts[q])){
+            if (config.keywords[keyword].exceptions.forEach(function(exc){if(RegExp(exc+"*").test(string)){continue;}})){}
+            else{ result[keyword] = true; }
+          }
+        }
+      }
+      for (var word in result){
+				/*Do whatever is needed*/
+      }
+    }
+  }
