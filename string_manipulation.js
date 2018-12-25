d8d9856defdaa5d61afff33639ded9e958517ab7
Bahamut731lp <heindrich73@gmail.com>
2018-12-25 17:50:04 +0100

    Update string_manipulation.js

--- string_manipulation.js
+++ string_manipulation.js
@@ -2,12 +2,12 @@ String.prototype.beautify = function(){
 	return this.toLowerCase().replace(/^\s+|\s+$|\s+(?=\s)/g, "")
 }
 
-String.prototype.massReplace = function(obj) { 
+String.prototype.massReplace = function(obj, flags) {
 	let find = Object.keys(obj);
 	let replace = Object.values(obj);
 	let replaceString = this;
 	  for (var i = 0; i < find.length; i++) {
-	    var regex = new RegExp(find[i]);
+	    var regex = new RegExp(find[i], flags);
 	    replaceString = replaceString.replace(regex, replace[i]);
 	  }
 	  return replaceString;
